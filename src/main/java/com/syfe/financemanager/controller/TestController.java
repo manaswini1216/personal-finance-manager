9a8117952941a3a02e96238a72ac2a753c09ae4d
manaswini1216 <manaswini1216@gmail.com>
2026-07-30 22:07:33 +0530

    Update TestController.java

--- src/main/java/com/syfe/financemanager/controller/TestController.java
+++ src/main/java/com/syfe/financemanager/controller/TestController.java
@@ -1,5 +1,5 @@
 package com.syfe.financemanager.controller;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
