309ab47a2766b6d59cc223845452a0227c821904
manaswini1216 <manaswini1216@gmail.com>
2026-07-30 22:02:23 +0530

    Update TestController.java

--- src/main/java/com/syfe/financemanager/controller/TestController.java
+++ src/main/java/com/syfe/financemanager/controller/TestController.java
@@ -3,6 +3,7 @@ package com.syfe.financemanager.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class TestController {
 
